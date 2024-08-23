ac361c8a48f0adc97f13d793c951c53c8be84c8a
gautam sharma <gautamsharma.gs48@gmail.com>
2024-08-23 22:49:10 +0530

    ch

--- src/common/components/home/becomeComponent.js
+++ src/common/components/home/becomeComponent.js
@@ -16,20 +16,21 @@ const data = [
 
 const BecomeComponent = () => {
   return (
-    <div className="bg-white grid grid-cols-3 gap-10 max-w-[1124px] mx-auto relative -top-10 relative ">
+    <div className="bg-white grid grid-cols-3 gap-10 max-w-[1124px] mx-auto relative -top-10 ">
       {data.map((ele, index) => (
-        <>
+        <div className="relative">
           <div
             key={index}
-            className="px-3 py-3 flex flex-col items-center gap-4 bg-white-10 rounded-lg bg-yellow-10"
+            className="px-3 py-3 flex flex-col items-center gap-4 bg-black-10 rounded-lg bg-yellow-10 shadow-md "
           >
             <h1 className="text-xl text-pretty font-semibold text-white-10 text-center whitespace-nowrap uppercase ">
               {ele?.title}
             </h1>
-
-            <button className="text-[#0c5397] text-sm font-bold  absolute bottom-0 top-10 bg-red-50 p-4 rounded-full">Know More</button>
           </div>
-        </>
+          <button className=" shadow-md text-sm font-bold hover:scale-110 top-10 bg-indigo-600 text-white-10 p-4 rounded-full flex items-center -mt-3 hover:underline mx-auto">
+            Know More
+          </button>
+        </div>
       ))}
       <div></div>
     </div>
