69f65e7046d81f38406d711c315259950003dd52
gautam sharma <gautamsharma.gs48@gmail.com>
2024-08-25 14:24:11 +0530

    changes

--- src/pages/api/user/create.ts
+++ src/pages/api/user/create.ts
@@ -1,12 +1,12 @@
-import { createUser } from "@/lib/actions/user";
+// import { createUser } from "@/lib/actions/user";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req:NextApiRequest, res: NextApiResponse) {
-    if (req.method !== "POST") {
-      return res.status(404).json({ msg: "wrong method" });
-    }
-    const data = req.body;
-    const userResponse = await createUser(data);
-    return res.status(200).json(userResponse);
+    // if (req.method !== "POST") {
+    //   return res.status(404).json({ msg: "wrong method" });
+    // }
+    // const data = req.body;
+    // const userResponse = await createUser(data);
+    // return res.status(200).json(userResponse);
   }
   
