d8a40c30a647e8eb0c1f9618607fdde90cdc801a
gautam sharma <gautamsharma.gs48@gmail.com>
2024-08-25 14:15:29 +0530

    changes

--- src/components/common/blur-typing.tsx
+++ src/components/common/blur-typing.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import BlurFadeText from "./magicui/blur-fade-text";
+import BlurFadeText from "../magicui/blur-fade-text";
 
 interface CombinedAnimationProps {
   text: string;
